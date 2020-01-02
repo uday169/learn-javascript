879b8ae81e3b117dae6236f9f5e8e473a3bfd352
Uday Pratap <udaypratap169@gmail.com>
2020-01-02 23:52:04 +0530

    coding challenge 1 Bmi Mark and John

--- js-basics/script.js
+++ js-basics/script.js
@@ -1,4 +1,22 @@
 // Hello World
 
-console.log('Hello World !!');
-alert('Hello World!!');
+// console.log('Hello World !!');
+// alert('Hello World!!');
+// coding challange 1
+
+
+var massMark = 66;//kg
+var massJohn = 68;
+var heightMark = 1.75;// meter
+var heightJohn = 1.81;
+
+var bmiMark = massMark /(heightMark*heightMark);
+
+var bmiJohn = massJohn /(heightJohn*heightJohn);
+
+var higherbmi = bmiMark > bmiJohn;
+
+console.log('Is mark bmi is higher than John ' +higherbmi);
+
+
+
