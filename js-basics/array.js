1abd248f6bef185a1766c7d9432a7c30579a8011
Uday Pratap <udaypratap169@gmail.com>
2020-02-05 22:50:36 +0530

    array example

---
+++ js-basics/array.js
@@ -0,0 +1,13 @@
+// this is array example
+
+var uday = ['Uday', 'Singh', 1997 , 'Developer', true];
+
+
+uday.push('india'); // push add the element in last of the array
+uday.unshift('Mr');// unsift add n first of the array
+console.log(uday);
+
+
+uday.pop(); //remove the last element of array
+uday.shift();//remoe the first elment of array
+console.log(uday);
