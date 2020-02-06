a098020a3a60e9468724dd84be5736e8677c8870
Uday Pratap <udaypratap169@gmail.com>
2020-02-07 00:40:22 +0530

    loop examples

---
+++ js-basics/loop.js
@@ -0,0 +1,16 @@
+// for loop and while loop example
+
+var uday = ['Uday', 'Singh', 1997 , 'Developer', true];
+
+console.log('for loop example:');
+
+for(i=0 ; i < uday.length ; i++){
+    console.log(uday[i]);
+}
+//
+console.log('while loop example:');
+var i = 0;
+while(i < uday.length){
+    console.log(uday[i]);
+    i++;
+}
