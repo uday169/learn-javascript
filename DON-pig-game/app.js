46e725ad13d1729e1a43f53f98820f1764772a02
Uday Pratap <udaypratap169@gmail.com>
2020-04-06 23:50:22 +0530

    Pig game update

--- DON-pig-game/app.js
+++ DON-pig-game/app.js
@@ -10,13 +10,91 @@ GAME RULES:
 */
 
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer, dice, gamePlaying;
 
+init();
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    
+    if(gamePlaying){
+        //1. Random number
+    dice = Math.floor(Math.random() * 6) + 1;
+
+    //2. Display the result
+
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    // 3. update the round score if the round score not 1
+
+    if (dice !== 1) {
+    //Add Score
+        roundScore += dice;
+        document.querySelector('#current-'+ activePlayer).textContent = roundScore;
+
+    } else {
+        // Next Player 
+       nextPlayer();
+    }
+    } 
+});
+
+document.querySelector('.btn-hold').addEventListener('click' ,function () { 
+    if (gamePlaying) {
+        // Add current score to GLOBAL Score.
+    scores[activePlayer] += roundScore;
+
+    // Update the UI
+    document.querySelector('#score-'+ activePlayer).textContent = scores[activePlayer];
+
+    // Check if player won the game
+    if (scores[activePlayer] >= 100) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-'+ activePlayer +'-panel').classList.add('winner');
+        document.querySelector('.player-'+ activePlayer +'-panel').classList.remove('active');
+        gamePlaying = false;
+    }else{
+        //Next Player
+        nextPlayer();
+    }
+
+    }
+});
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
+gamePlaying = true;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
+document.querySelector('.dice').style.display = 'none';
 
-document.querySelector('#current-'+ activePlayer).textContent = dice;
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+document.getElementById('name-0').textContent = 'Player 1';
+document.getElementById('name-1').textContent = 'Player 2';
+document.querySelector('.player-0-panel').classList.remove('winner');
+document.querySelector('.player-1-panel').classList.remove('winner');
+document.querySelector('.player-0-panel').classList.remove('active');
+document.querySelector('.player-1-panel').classList.remove('active');
+document.querySelector('.player-0-panel').classList.add('active');
+}
