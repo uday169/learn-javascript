66efc0726a000499e5e92a511a137a9de18b134d
Uday Pratap <udaypratap169@gmail.com>
2020-05-18 23:35:23 +0530

    Function Constructor

---
+++ Objects/script.js
@@ -0,0 +1,11 @@
+// Function Constructor
+
+
+var Person = function(name,yearOfBirth,job){
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+var uday = new Person('Uday', 1997, 'Engineer');
+
