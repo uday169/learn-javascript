8144c9d9626b9836605e7dfa36e008aac5e782cd
Uday Pratap <udaypratap169@gmail.com>
2020-05-18 23:45:43 +0530

    Prototype property of function constructor

--- Objects/script.js
+++ Objects/script.js
@@ -7,5 +7,14 @@ var Person = function(name,yearOfBirth,job){
     this.job = job;
 }
 
-var uday = new Person('Uday', 1997, 'Engineer');
+Person.prototype.calcAge = function () {
+    console.log(2020-this.yearOfBirth);
+}
 
+var uday = new Person('Uday', 1997, 'Engineer');
+var rohan = new Person ('Rohan',1995, 'Teacher');
+var raju = new Person ('Raju',2000, 'Builder');
+
+uday.calcAge();
+rohan.calcAge();
+raju.calcAge();
